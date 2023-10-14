9e071c7d291859fa9b522689dfd250bdea474d2d
unknown <shahrukhamin27@gmail.com>
2023-10-15 00:38:37 +0600

    updated

--- lesson4/src/main.ts
+++ lesson4/src/main.ts
@@ -1,89 +1,38 @@
-let stringArr = ['one', 'hey', 'Dave']
+//Type Aliases and Interface
 
-let guitars = ['Strat', 'Les Paul', 5150]
+type User=string;
 
-let mixedData = ['EVH', 1984, true]
-
-// As stringArr defined with string onlyy so it accepts string only and guitars array has strin and number both type so accepts both
-stringArr[0]='Shaik'
-guitars.push(12)
-
-const test=[]  //accepts any type of data
-let bands:string[]=[]
-bands.push('amin')
-console.log(bands)
-
-// Tuple :=specific position a ki type  data boshbey shei jnno
-let myTuple: [string, number, boolean] = ['Dave', 42, true]
-
-myTuple[0]="shah"
-
-console.log(myTuple)
-
-
-// Objects with TS
-
-interface User {
-    name:String;
-    age:Number|String;
-    id:(String)[]
+type newUser={
+    name:string;
+    email:string;
+    age?:number;
+    // getMessage():void;
 }
 
-// Now create object
-const user1:User={
-    name:"shaik",
-    age:31,
-    id:['1']
+type useDetails={
+    jobDetail:string
 }
 
-const user2:User={
-    name:"Mewao",
-    age:'22',
-    id:['2']
+type combinedInfo=newUser & useDetails;
+
+interface UserInterface{
+    name:string;
+    email:string;
+    age:number;
+    getMessage():string;
 }
 
-console.log(user1,user2)
+const userName:User='Shaik';
+console.log(userName);
 
-// Here object interface created but name? means obj with using the interface can have name parameter or not
-interface Guitarist {
-    name?: string,   //property optional
-    active: boolean,
-    albums: (string | number)[]
+const createNewUser=(user:combinedInfo)=>{
+    console.log('user Info',user);
 }
 
-let evh: Guitarist = {
-    name: 'Eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812']
-}
-
-let jp: Guitarist = {
-    active: true,
-    albums: ['I', 'II', 'IV']
-}
-
-console.log(evh,jp)
-
-
-//function
-
-const testFuntion=(payload:Guitarist)=>{
-    console.log(`name of the albums: ${payload?.albums}`)
-}
-
-testFuntion(jp)
-
-
-
-// Enums 
-// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
-
-enum Grade {
-    U = 100,
-    D,
-    C,
-    B,
-    A,
-}
-
-console.log(Grade.C)
+createNewUser({
+    name:'Shaik',
+    email:'sha@gmail.com',
+    age:25,
+    jobDetail:'Software Engineer'
+})
+//--------------------------------------------------------------
